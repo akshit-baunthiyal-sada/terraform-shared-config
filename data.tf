932286cae250376d5349fa68d5f46e9472b2d25a
Akshit Baunthiyal <akshit.baunthiyal@sada.com>
2025-09-22 19:13:22 +0530

    Back to hardcoding but output multiple JSON files

--- data.tf
+++ data.tf
@@ -1,5 +1,10 @@
 # Read from GCS bucket
 data "google_storage_bucket_object_content" "app_config" {
-  name   = var.file
+  name   = var.app_config_file
+  bucket = var.bucket
+}
+
+data "google_storage_bucket_object_content" "terraform_state" {
+  name   = var.terraform_state_file
   bucket = var.bucket
 }
